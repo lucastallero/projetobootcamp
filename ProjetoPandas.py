21d5c02466df52d1ea6a06b4d67139b64a87ef72
Lucas <lucas.tallero@gmail.com>
2022-10-10 16:40:05 +0100

    desafios

---
+++ ProjetoPandas.py
@@ -0,0 +1,12 @@
+# # Desafio posição alfabeto
+
+# alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+# valor = input()
+# posicao = alfabeto.index(valor) + 1
+# print(posicao)
+
+# Desafio papagaio
+
+perna_levantada = input()
+
+print(perna_levantada)
